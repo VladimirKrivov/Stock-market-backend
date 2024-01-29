d2b4b68054967c753bd66901e225c52e05cc060d
Vladimir Krivov <vov.krv@yandex.ru>
2024-01-30 01:43:16 +0300

    Fix calc Stocks Growth

--- src/main/java/stock/market/backend/app/services/HistoryService.java
+++ src/main/java/stock/market/backend/app/services/HistoryService.java
@@ -3,7 +3,6 @@ package stock.market.backend.app.services;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import stock.market.backend.app.exeption.NotFoundException;
 import stock.market.backend.app.models.dto.HistoryDto;
 import stock.market.backend.app.models.dto.ShortHistoryDto;
 import stock.market.backend.app.models.dto.StockDto;
@@ -19,10 +18,8 @@ import stock.market.backend.app.util.Parser;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -33,17 +30,12 @@ public class HistoryService {
     private final HistoryElemRepositories historyElemRepositories;
     private final Parser parser;
     private final Mapper mapper;
-    private final StocksService stocksService;
     private final StockFromDateService stockFromDateService;
     private final UserRepositories userRepositories;
 
     public HistoryDto calcPricesStocks(ShortHistoryDto dto) {
 
         History history = new History();
-
-
-        List<StockFromDate> stockFromDateList = new ArrayList<>();
-        List<HistoryElem> historyElems = new ArrayList<>();
 
         LocalDate from = parser.parseDate(dto.getFrom());
         LocalDate till = parser.parseDate(dto.getTill());
@@ -62,13 +54,54 @@ public class HistoryService {
 
         List<StockDto> stockDto = dto.getStocksList();
 
+        List<HistoryElem> allHistoryElems = new ArrayList<>();
+
         for (StockDto elem : stockDto) {
-            stockFromDateList.addAll(stockFromDateService.getHistory(elem.getShortname(), dto.getUserName(), dto.getFrom(), dto.getTill()));
+            List<StockFromDate> dtoDate = new ArrayList<>(stockFromDateService.getHistory(elem.getShortname(), dto.getUserName(), dto.getFrom(), dto.getTill()));
+            allHistoryElems.addAll(calcGrowth(dtoDate));
         }
 
-        for (int i = 1; i < stockFromDateList.size(); i++) {
-            StockFromDate stockFromDate1 = stockFromDateList.get(i - 1);
-            StockFromDate stockFromDate2 = stockFromDateList.get(i);
+        for (HistoryElem elem : allHistoryElems) {
+            elem.setHistory(history);
+        }
+
+        // Найти топ дня из списка акций HistoryElement
+        List<HistoryElem> newHistoryElems = findProfitable(allHistoryElems);
+
+
+        List<Double> res = new ArrayList<>();
+        for (int i = 0; i < newHistoryElems.size(); i++) {
+            double buf = newHistoryElems.get(i).getGrowth() + 1;
+            res.add(buf);
+        }
+
+        Double mult = res.stream()
+                .mapToDouble(a -> a)
+                .reduce(1, (a, b) -> a * b);
+
+        Double result = (mult * 365 / calendarDays);
+
+        history.setResult(result);
+
+
+        History newHistory = historyRepository.save(history);
+        log.info("Сохранили Hystory: {}", history);
+
+
+        historyElemRepositories.saveAll(newHistoryElems);
+
+
+        newHistory.setHistoryElem(historyElemRepositories.findAllByHistory_Id(newHistory.getId()));
+
+
+        return mapper.historyToHistoryDto(newHistory);
+    }
+
+    private List<HistoryElem> calcGrowth(List<StockFromDate> dtoDate) {
+        List<HistoryElem> historyElems = new ArrayList<>();
+        for (int i = 1; i < dtoDate.size(); i++) {
+            StockFromDate stockFromDate1 = dtoDate.get(i - 1);
+            StockFromDate stockFromDate2 = dtoDate.get(i);
 
             Double growthCalc = (stockFromDate2.getClose() - stockFromDate1.getClose()) / stockFromDate1.getClose();
 
@@ -76,39 +109,49 @@ public class HistoryService {
             historyElem.setDate(stockFromDate2.getTradeDate());
             historyElem.setShortName(stockFromDate2.getShortName());
             historyElem.setGrowth(growthCalc);
-            historyElem.setHistory(history);
 
             historyElems.add(historyElem);
         }
 
-        Double result = historyElems.get(0).getGrowth();
+        return historyElems;
+    }
 
-        for (int i = 1; i < historyElems.size(); i++) {
-            result = result * historyElems.get(i).getGrowth();
+    private List<HistoryElem> findProfitable(List<HistoryElem> historyElems) {
+        // Создаем Map, где ключом является дата, а значением - объект класса HistoryElem
+        Map<LocalDate, HistoryElem> historyElemsByDate = new HashMap<>();
+
+        // Проходимся по списку объектов класса HistoryElem
+        for (int i = 0; i < historyElems.size(); i++) {
+            // Получаем дату объекта
+            LocalDate date = historyElems.get(i).getDate();
+
+            // Если в Map уже есть список для данной даты, то сравниваем их growth и заменяем на объект с наибольшим
+            //значением
+            if (historyElemsByDate.containsKey(date)) {
+
+                historyElemsByDate.get(date);
+
+                if (historyElemsByDate.get(date).getGrowth() < historyElems.get(i).getGrowth()) {
+                    historyElemsByDate.put(date, historyElems.get(i));
+                }
+            }
+            // Если в Map нет списка для данной даты, то просто добавляем объект
+            else {
+                historyElemsByDate.put(date, historyElems.get(i));
+            }
         }
 
-        result = result + 1;
+        List<HistoryElem> newHistoryElems = new ArrayList<>();
 
-        result = ((result * (365/calendarDays)) - 1) * 100;
+        for (Map.Entry<LocalDate, HistoryElem> entry : historyElemsByDate.entrySet()) {
+            newHistoryElems.add(entry.getValue());
+        }
 
-        System.out.println("Результат");
-        System.out.println(result);
+        newHistoryElems.sort(Comparator.comparing(HistoryElem::getDate));
 
-        history.setResult(result);
-
-        History newHistory = historyRepository.save(history);
-        log.info("Сохранили Hystory: {}", history);
-
-        historyElemRepositories.saveAll(historyElems);
+        return newHistoryElems;
 
 
-        newHistory.setHistoryElem(historyElemRepositories.findAllByHistory_Id(newHistory.getId()));
-
-
-        System.out.println(newHistory.getHistoryElem());
-
-
-        return mapper.historyToHistoryDto(newHistory);
     }
 
     public List<HistoryDto> findHistoryToUser(String username) {
